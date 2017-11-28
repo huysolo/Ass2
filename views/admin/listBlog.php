2d320048ebc1f8153e49171aab7ce6749a8d49a2
DESKTOP-0FFOSI4\Solo <tuthanhsolo@gmail.com>
2017-11-28 18:38:24 +0700

    Edit database
    Alter blog table

--- views/admin/listBlog.php
+++ views/admin/listBlog.php
@@ -5,3 +5,20 @@
  * Date: 22/11/2017
  * Time: 8:21 CH
  */
+include "../layout/adminHeader.php";
+include  '../../connect.php';
+$result = $conn->query("SELECT * FROM BLOG");
+?>
+
+<div class="container">
+    <div class="col-md-12">
+    <?php
+        if($result->num_rows!=0){
+            while ($rows = $result->fetch_assoc()){
+                echo "<h1>". $rows['Title'] . "</h1>";
+                //echo "<p>" . $rows['summary'] ."</p>";
+            }
+        }
+    ?>
+    </div>
+</div>
